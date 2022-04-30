f032794e9b9df241b6ea054f315c653e935cbe07
gerrygeop <gerryja28@gmail.com>
2022-05-01 01:17:46 +0800

    prod

--- resources/views/components/card-afiliasi.blade.php
+++ resources/views/components/card-afiliasi.blade.php
@@ -4,7 +4,7 @@
             <img class="w-auto max-h-full" src="{{ $image }}" alt="avatar">
         </div>
         
-        <div class="pb-4 bg-blue-700/60 backdrop-blur absolute border -bottom-10 inset-x-0 h-0 group-hover:bottom-0 group-hover:h-full lg:py-2 transition-all duration-200">
+        <div class="pb-4 bg-blue-600/70 backdrop-blur-sm absolute border -bottom-10 inset-x-0 h-0 group-hover:bottom-0 group-hover:h-full lg:py-2 transition-all duration-200">
             <h2 class="flex items-center justify-center h-full text-lg text-slate-100 font-semibold tracking-wide">
                 <span>{{ $slot }}</span>
             </h2>
